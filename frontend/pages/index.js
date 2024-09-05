90aa293771a361946f320ba1c0514eeb24422c8e
Shashank Sharma <79395827+valyrian24052@users.noreply.github.com>
2024-09-05 20:10:43 +0530

    aaaa

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,13 +3,12 @@ import styles from '../styles/Home.module.css';
 import Logo from '../utils/logo.svg';
 import Send from '../utils/sendbutton.svg';
 import Head from 'next/head';
+import DOMPurify from 'dompurify';  // Add DOMPurify for security
 
 export default function Home() {
     const [isActive, setIsActive] = useState(false);
     const [userInput, setUserInput] = useState('');
     const [conversation, setConversation] = useState([]);
-    
-    
     const conversationEndRef = useRef(null);
 
     useEffect(() => {
@@ -19,21 +18,13 @@ export default function Home() {
     }, [conversation]);
 
     useEffect(() => {
-        console.log('useEffect triggered');
-
         const script = document.createElement('script');
         script.src = '/particles.js'; 
         script.async = true;
 
         script.onload = () => {
-            console.log('Particles.js script loaded');
             if (window.particlesJS) {
-                console.log('particlesJS is available');
-                window.particlesJS.load('particles-js', '/particles-config.json', () => {
-                    console.log('Particles.js config loaded');
-                });
-            } else {
-                console.error('particlesJS is not available');
+                window.particlesJS.load('particles-js', '/particles-config.json', () => {});
             }
         };
 
@@ -42,7 +33,6 @@ export default function Home() {
         };
 
         document.body.appendChild(script);
-        console.log('Script appended to body');
     }, []);
 
     const handleSend = async () => {
@@ -61,7 +51,6 @@ export default function Home() {
         try {
             let response;
             if (isActive) {
-                // Send request to assistant
                 response = await fetch('/api/assistant', {
                     method: 'POST',
                     headers: {
@@ -70,7 +59,6 @@ export default function Home() {
                     body: JSON.stringify(payload),
                 });
             } else {
-                // Send request to runpython
                 response = await fetch('/api/runpython', {
                     method: 'POST',
                     headers: {
@@ -105,7 +93,6 @@ export default function Home() {
 
     const toggleSwitch = () => {
         setIsActive(!isActive);
-        
     };
 
     return (
@@ -134,18 +121,18 @@ export default function Home() {
                         <h1 className={styles.mainTitle}>Hi, I am AIRED</h1>
                         <p className={styles.subTitle}>{title}</p>
                         <div className={styles.chatContainer}>
-                        <div 
-                            className={styles.clickableText} 
-                            onClick={() => handleTextClick(isActive ? 'List me Shashank\'s experiences' : 'Tell me a Joke')}
-                        >
-                            {isActive ? 'List me Shashank\'s experiences' : 'Tell me a Joke'}
-                        </div>
-                        <div 
-                            className={styles.clickableText} 
-                            onClick={() => handleTextClick(isActive ? 'List me tech stacks Shashank is proficient in' : 'Tell me a Bed time story')}
-                        >
-                            {isActive ? 'List me tech stacks Shashank is proficient in' : 'Tell me a Bed time story'}
-                        </div>
+                            <div 
+                                className={styles.clickableText} 
+                                onClick={() => handleTextClick(isActive ? 'List me Shashank\'s experiences' : 'Tell me a Joke')}
+                            >
+                                {isActive ? 'List me Shashank\'s experiences' : 'Tell me a Joke'}
+                            </div>
+                            <div 
+                                className={styles.clickableText} 
+                                onClick={() => handleTextClick(isActive ? 'List me tech stacks Shashank is proficient in' : 'Tell me a Bed time story')}
+                            >
+                                {isActive ? 'List me tech stacks Shashank is proficient in' : 'Tell me a Bed time story'}
+                            </div>
                             <div className={styles.inputContainer}>
                                 <input 
                                     type="text" 
@@ -175,25 +162,29 @@ export default function Home() {
                                     key={index} 
                                     className={`${styles.conversationItem} ${item.type === 'user' ? styles.user : styles.assistant}`}
                                 >
-                                    {item.text}
+                                    {item.type === 'assistant' ? (
+                                        <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(item.text) }} />
+                                    ) : (
+                                        item.text
+                                    )}
                                 </div>
                             ))}
                             {/* Ref to ensure scrolling to the last message */}
                             <div ref={conversationEndRef} />
                         </div>
                         <div className={styles.chatContainer}>
-                        <div 
-                            className={styles.clickableText} 
-                            onClick={() => handleTextClick(isActive ? 'List me Shashank\'s experiences' : 'Tell me a Joke')}
-                        >
-                            {isActive ? 'List me Shashank\'s experiences' : 'Tell me a Joke'}
-                        </div>
-                        <div 
-                            className={styles.clickableText} 
-                            onClick={() => handleTextClick(isActive ? 'List me tech stacks Shashank is proficient in' : 'Tell me a Bed time story')}
-                        >
-                            {isActive ? 'List me tech stacks Shashank is proficient in' : 'Tell me a Bed time story'}
-                        </div>
+                            <div 
+                                className={styles.clickableText} 
+                                onClick={() => handleTextClick(isActive ? 'List me Shashank\'s experiences' : 'Tell me a Joke')}
+                            >
+                                {isActive ? 'List me Shashank\'s experiences' : 'Tell me a Joke'}
+                            </div>
+                            <div 
+                                className={styles.clickableText} 
+                                onClick={() => handleTextClick(isActive ? 'List me tech stacks Shashank is proficient in' : 'Tell me a Bed time story')}
+                            >
+                                {isActive ? 'List me tech stacks Shashank is proficient in' : 'Tell me a Bed time story'}
+                            </div>
                             <div className={styles.inputContainer}>
                                 <input 
                                     type="text" 
